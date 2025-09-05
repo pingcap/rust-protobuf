27a459873f34556c3a5c6c9e0f0b733f7a81ab93
Jianjun Liao <jianjun.liao@outlook.com>
2025-09-05 18:36:53 +0800

    impl pbprint for chars
    Signed-off-by: Jianjun Liao <jianjun.liao@outlook.com>

--- protobuf/src/chars.rs
+++ protobuf/src/chars.rs
@@ -5,6 +5,7 @@ use std::str;
 use bytes::Bytes;
 
 use clear::Clear;
+use crate::core::PbPrint;
 
 /// Thin wrapper around `Bytes` which guarantees that bytes are valid UTF-8 string.
 #[derive(Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
@@ -77,6 +78,14 @@ impl fmt::Debug for Chars {
     #[inline]
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         fmt::Debug::fmt(&**self, f)
+    }
+}
+
+#[cfg(feature = "bytes")]
+impl PbPrint for Chars {
+    #[inline]
+    fn fmt(&self, name: &str, buf: &mut String) {
+        self.0.fmt(name, buf);
     }
 }
 
